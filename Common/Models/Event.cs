1eb18b30063200b2f759605dfde45b9281c6f50a
Ryan Attard <ryanattard@gmail.com>
2026-02-24 11:13:20 +0100

    Add project files.

---
+++ Common/Models/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Models
+{
+    public class Event
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool Public { get; set; }
+        public int MaximumTickets { get; set; }
+        public string FilePath { get; set; }
+        public string Organiser { get; set; }
+    }
+}
---
+++ Common/Models/Ticket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Models
+{
+    public enum Status { Paid, Cancelled, Used }
+    public class Ticket
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        [ForeignKey("Event")]
+        public int EventFK { get; set; }
+        public virtual Event Event { get; set; }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string IdCard { get; set; }
+
+        public Status Status { get; set; }
+
+
+    }
+}
